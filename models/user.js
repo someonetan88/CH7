e8b770d469141a33ded81bd958de22087109618f
someonetan88 <156898921+someonetan88@users.noreply.github.com>
2024-05-17 18:12:19 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,12 +5,10 @@ function findUserByEmail(email) {
   return users.find((user) => user.email === email);
 }
 
-// Function to hash password
 async function hashPassword(password) {
   return await bcrypt.hash(password, 10);
 }
 
-// Function to update user's password
 function updateUserPassword(email, hashedPassword) {
   const user = findUserByEmail(email);
   if (user) {
